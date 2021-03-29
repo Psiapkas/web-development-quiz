d65a6efcbf1c2510f23d828aeadb68c7dae8509b
unknown <peter.siapkas@gmail.com>
2021-03-29 11:47:03 +1100

    next question function not working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,43 +5,66 @@ var answerFour = document.getElementById ("answerFour");
 var timer = document.getElementById ("timer");
 var highscore = document.getElementById("highscore");
 var testQuestion = document.getElementById("question");
-var startGame = document.getElementById("startGame")
+var startGame = document.getElementById("startGame");
+var questionResponse = document.getElementById("questionAnswers");
 var timeLeft = 30
 // To do :
 // create questions and lable the correct answer
-// write code that cyles through the question
 // write code that replaces buttons with answers
+// write code that cyles through the question
 // write code that checks if answer is correct and adds time or subtracts time 
 // save info 
 // when user clicks 'highscore' a window prompt should show up displaying score. 
-var question1 = {
+
+// all questions stored as objects inside variable 
+var questionSelect = [{
     query: "what is a function ?",
     response: ["A reusable set of instructions", "Elements of html", "An array of variables", "cheese" ],
     answer: "A reusable set of instructions"
- }
-
-var question2 = {
+ },
+ {
     query:  "what is html ?",
     response: ["carrot", "the base of a website","the base of chrome OS", "a videogame" ],
     answer: "the base of a website"
 }
-var currentQuestion = 0
-var questionSelect = [question1, question2]
-var 
+]
+var questionNumber = 0
+var quiz = questionSelect[questionNumber]
+
+
 function gameStart (){
+    mainGame();
     countdown();
 
 }
 
-function questionChange() {
-
+function mainGame() {
+ if(quiz) {
+   testQuestion.textContent = quiz.query
+   answerOne.textContent = quiz.response[0]
+   answerTwo.textContent = quiz.response[1]
+   answerThree.textContent = quiz.response[2]
+   answerFour.textContent = quiz.response[3]
+ }
   
 }
-function displayMessage(){
-    if (timer === 0){
-        prompt = "you ran out of time";
-        return;
-    }
+// checks answer and moves to next question 
+function correctAnswer () {
+
+ if (questionResponse === quiz.answer ){
+
+   questionNumber++;
+   timer+5
+ } else {
+   alert = "you lost"
+ }
+
+}
+// stops time and saves initals and score
+function gameFinish () {
+  countdown(clearInterval);
+ 
+
 }
 
 function countdown() { 
@@ -53,11 +76,23 @@ function countdown() {
       clearInterval(timeInterval);
       timer.textContent = " ";
       displayMessage();
+
     }
 
     
 
-  }, 1000);
+  }, 2000);
 }
- 
+function displayMessage(){
+  if (timer === 0){
+      prompt = "you ran out of time";
+      return;
+  }
+}
+ function checkscore(){
+
+ }
+
+
 startGame = addEventListener("click", gameStart)
+highscore = addEventListener("click", checkscore)
